86bdfa157226bb953cc59dcefba51b8a90972d51
Egor Mzln <egormizulin@mail.ru>
2024-11-18 16:06:18 +0300

    Initial Commit

---
+++ UiKitRun6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKitRun6
+//
+//  Created by Егор Мизюлин on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
