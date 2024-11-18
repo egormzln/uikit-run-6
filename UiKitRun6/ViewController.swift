d1e000a1ec31fe1733158e1d7b8710c062ca0306
Egor Mzln <egormizulin@mail.ru>
2024-11-18 16:12:31 +0300

    fix

--- UiKitRun6/ViewController.swift
+++ UiKitRun6/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var animator: UIDynamicAnimator!
+    var snap: UISnapBehavior!
+
+    private let squareView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setupSquareView()
+        setupTap()
     }
 
+    private func setupSquareView() {
+        view.addSubview(squareView)
+        squareView.frame = .init(x: 0, y: 0, width: 80, height: 80)
+        squareView.center = view.center
+        squareView.layer.cornerRadius = 10
+        squareView.layer.masksToBounds = true
+        squareView.backgroundColor = .systemBlue
+    }
 
+    private func setupTap() {
+        animator = UIDynamicAnimator(referenceView: view)
+        snap = UISnapBehavior(item: squareView, snapTo: squareView.center)
+        snap.damping = 1
+        animator.addBehavior(snap)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc private func handleTap(_ sender: UITapGestureRecognizer) {
+        let point = sender.location(in: view)
+        snap.snapPoint = point
+    }
 }
-
